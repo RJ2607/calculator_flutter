6d29ecc5d72adc01309e000b65574620297cf625
RJ2607 <rishijain2607@gmail.com>
2023-05-05 20:01:42 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:calculator/colors.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,7 +19,36 @@ class _calculatorappState extends State<calculatorapp> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Column(children: [Expanded(child: Container(color: Colors.red))]),
+      body: Column(
+        children: [
+          Expanded(child: Container(color: Colors.red)),
+          Row(
+            children: [button(text: "2")],
+          ),
+        ],
+      ),
     );
+  }
+
+  Widget button({text, tColor = Colors.white, buttonbgcolor = buttonColor}) {
+    return Expanded(
+        child: Container(
+      margin: const EdgeInsets.all(8),
+      child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12)),
+              padding: EdgeInsets.all(22),
+              primary: buttonbgcolor),
+          onPressed: () {},
+          child: Text(
+            text,
+            style: TextStyle(
+              fontSize: 18,
+              color: tColor,
+              fontWeight: FontWeight.bold,
+            ),
+          )),
+    ));
   }
 }
