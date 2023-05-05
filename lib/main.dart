7b36aff29439387e81e4d95e0ff90cfdf52e89c5
RJ2607 <rishijain2607@gmail.com>
2023-05-05 20:21:38 +0530

    1

--- lib/main.dart
+++ lib/main.dart
@@ -21,9 +21,64 @@ class _calculatorappState extends State<calculatorapp> {
       backgroundColor: Colors.black,
       body: Column(
         children: [
-          Expanded(child: Container(color: Colors.red)),
+          Expanded(
+              child: Container(
+                  padding: const EdgeInsets.all(12),
+                  color: Colors.red,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: const [Text("Input")],
+                  ))),
           Row(
-            children: [button(text: "2")],
+            children: [
+              button(
+                  text: "AC",
+                  buttonbgcolor: operatorColor,
+                  tColor: orangeColor),
+              button(
+                  text: "<-",
+                  buttonbgcolor: operatorColor,
+                  tColor: orangeColor),
+              button(text: "", buttonbgcolor: Colors.transparent),
+              button(
+                  text: "/", buttonbgcolor: operatorColor, tColor: orangeColor),
+            ],
+          ),
+          Row(
+            children: [
+              button(text: "7"),
+              button(text: "8"),
+              button(text: "9"),
+              button(
+                  text: "x", buttonbgcolor: operatorColor, tColor: orangeColor),
+            ],
+          ),
+          Row(
+            children: [
+              button(text: "4"),
+              button(text: "5"),
+              button(text: "6"),
+              button(
+                  text: "-", buttonbgcolor: operatorColor, tColor: orangeColor),
+            ],
+          ),
+          Row(
+            children: [
+              button(text: "1"),
+              button(text: "2"),
+              button(text: "3"),
+              button(
+                  text: "+", buttonbgcolor: operatorColor, tColor: orangeColor),
+            ],
+          ),
+          Row(
+            children: [
+              button(
+                  text: "%", buttonbgcolor: operatorColor, tColor: orangeColor),
+              button(text: "0"),
+              button(text: "."),
+              button(text: "=", buttonbgcolor: Colors.red),
+            ],
           ),
         ],
       ),
